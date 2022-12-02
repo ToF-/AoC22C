83f31efaf4ecee4ac6cf8a9d9a4abffe6fee3696
Christophe Thibaut <cthibaut@octo.com>
2022-12-02 08:24:00 +0100

    day 02

--- day02/rock_paper_scissors.c
+++ day02/rock_paper_scissors.c
@@ -2,7 +2,45 @@
 #include <errno.h>
 #include <string.h>
 const int LINE_MAX = 5;
-
+// match = your opp' choice * 10 + your choice
+// score = your choice + 0:loss, 3:draw, 6:win
+int part1Scorer(int match) {
+    int score;
+    switch(match) { 
+        case 11: score = 1 + 3; break;
+        case 12: score = 2 + 6; break;
+        case 13: score = 3 + 0; break;
+        case 21: score = 1 + 0; break;
+        case 22: score = 2 + 3; break;
+        case 23: score = 3 + 6; break;
+        case 31: score = 1 + 6; break;
+        case 32: score = 2 + 0; break;
+        case 33: score = 3 + 3; break;
+        default: fprintf( stderr, "error: %d \n", score);
+                 return 1;
+    }
+    return score;
+}
+// match = your opp' choice * 10 + your wanted result (0 : loss, 1: draw, 2: win)
+// complete the score with required choice in order to obtain result
+// score = required choice + 0:loss, 3:draw, 6:win
+int part2Scorer(int match) {
+    int score;
+    switch(match) { 
+        case 11: score = 3 + 0; break;
+        case 12: score = 1 + 3; break;
+        case 13: score = 2 + 6; break;
+        case 21: score = 1 + 0; break;
+        case 22: score = 2 + 3; break;
+        case 23: score = 3 + 6; break;
+        case 31: score = 2 + 0; break;
+        case 32: score = 3 + 3; break;
+        case 33: score = 1 + 6; break;
+        default: fprintf( stderr, "error: %d \n", score);
+                 return 1;
+    }
+    return score;
+}
 int main(int argc, char *argv[]) {
     char line[LINE_MAX];
     FILE *puzzleFile;
@@ -14,6 +52,7 @@ int main(int argc, char *argv[]) {
     }
     char opp, you;
     int total = 0;
+    int soughtTotal = 0;
     while(fgets(line, LINE_MAX, puzzleFile)) {
         if ( ferror( puzzleFile ) ) {
             fprintf( stderr, "Reading error with code %d\n", errno );
@@ -22,7 +61,6 @@ int main(int argc, char *argv[]) {
         line[strcspn(line, "\n")] = 0;
         sscanf(line,"%c %c", &opp, &you);
         int match = 0;
-        int score = 0;
         printf("%s ", line);
         switch(opp) {
             case 'A': match = 10; break;
@@ -38,21 +76,11 @@ int main(int argc, char *argv[]) {
             default: fprintf( stderr, "error: %c \n", you);
                      return 1;
         }
-        switch(match) {
-            case 11: score = 1 + 3; break;
-            case 12: score = 2 + 6; break;
-            case 13: score = 3 + 6; break;
-            case 21: score = 1 + 0; break;
-            case 22: score = 2 + 3; break;
-            case 23: score = 3 + 6; break;
-            case 31: score = 1 + 0; break;
-            case 32: score = 2 + 0; break;
-            case 33: score = 3 + 3; break;
-            default: fprintf( stderr, "error: %d \n", score);
-                     return 1;
-        }
+        int score = part1Scorer(match);
+        int soughtScore = part2Scorer(match);
         total += score;
-        printf("%d, %d, %d\n", match, score, total);
+        soughtTotal += soughtScore;
+        printf("%d, %d, %d, %d, %d\n", match, score, total, soughtScore, soughtTotal);
     }
     return 0;
 }
