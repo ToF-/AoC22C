ccb173dc496993ccdfb74b2d10926486d8b2a590
Christophe Thibaut <cthibaut@octo.com>
2022-12-07 22:16:31 +0100

    refactoring day07

--- day07/no_space_left.c
+++ day07/no_space_left.c
@@ -34,27 +34,21 @@ void print_file_system(struct node* node, int level) {
         print_file_system(node->siblings, level);
 }
 
-struct node *new_dir(struct node* parent, char *dir_name) {
+struct node *new_node(char *name, long size) {
     struct node *node = malloc(sizeof(struct node));
-    node->name = malloc(strlen(dir_name)+1);
-    strcpy(node->name, dir_name);
-    node->type = DIR_NODE;
-    node->size = -1;
-    node->parent = parent;
+    node->name = malloc(strlen(name)+1);
+    memset(node->name, 0, strlen(name)+1);
+    strcpy(node->name, name);
+    node->type = size == -1 ? DIR_NODE : FILE_NODE;
+    node->size = size;
+    node->parent = NULL;
     node->siblings = NULL;
     node->children = NULL;
     return node;
 }
-
 struct node *add_node(struct node *parent, char *name, long size) {
-    struct node *node = malloc(sizeof(struct node));
-    node->name = malloc(strlen(name)+1);
-    strcpy(node->name, name);
-    node->type = size == -1 ? DIR_NODE : FILE_NODE;
-    node->size = size;
+    struct node *node = new_node(name, size);
     node->parent = parent;
-    node->siblings = NULL;
-    node->children = NULL;
     if(node->parent->children == NULL) {
         node->parent->children = node;
     } else {
@@ -71,14 +65,13 @@ struct node *add_node(struct node *parent, char *name, long size) {
     return node;
 }
 
-struct node *change_dir(struct node *current, char *name) {
-    struct node *node;
-    if(current->type != DIR_NODE)
+struct node *change_dir(struct node *file_system, struct node *current, char *name) {
+    if(current == NULL)
         return NULL;
-    if(!strcmp(name, "..")) {
-        return current->parent;
-    }
-    node = current->children;
+    if(current->type != DIR_NODE) return NULL;
+    if(!strcmp(name, "/")) return file_system;
+    if(!strcmp(name, "..")) return current->parent;
+    struct node *node = current->children;
     while(node != NULL) {
         if(!strcmp(node->name, name)) {
             return node;
@@ -140,71 +133,41 @@ int main(int argc, char *argv[]) {
     FILE *puzzleFile;
     char line[LINE_MAX];
 
-    struct node *file_system = (struct node *)malloc(sizeof(struct node));
-    struct node *current;
-
-    if(argc == 1) {
-        file_system = new_dir(file_system, "/");
-        current = file_system;
-        struct node *dir = current;
-        current = add_node(dir, "a", -1);
-        current = add_node(dir, "b.txt", 14848514);
-        current = add_node(dir, "c.dat", 8504156);
-        current = add_node(dir, "d", -1);
-        current = file_system;
-        current = change_dir(current, "a");
-	dir = current;
-	current = add_node(dir, "e", -1);
-	current = add_node(dir, "f", 29116);
-	current = add_node(dir, "g", 2557);
-	current = add_node(dir, "h.lst", 62596);
-	current = change_dir(dir, "e");
-	dir = current;
-	current = add_node(dir, "i", 584);
-	dir = dir->parent;
-	dir = dir->parent;
-	dir = change_dir(dir, "d");
-	current = add_node(dir, "j", 4060174);
-	current = add_node(dir, "d.log", 8033020);
-	current = add_node(dir, "d.ext", 5626152);
-	current = add_node(dir, "k", 7214296);
-        print_file_system(file_system, 0);
-        destroy_node(file_system);
-        return 0;
-    }
     puzzleFile = fopen(argv[1], "r");
     if(puzzleFile == NULL) {
         fprintf(stderr, "can't open file %s\n", argv[1]);
         return 1;
     }
-    int ls_in_progress = false;
-    add_node(file_system, "/", -1);
-    current = file_system;
+    struct node *file_system = new_node("/", -1);
+    struct node *current = file_system;
     while(fgets(line, LINE_MAX, puzzleFile)) {
         line[strcspn(line, "\n")] = 0;
         const char *sep = " ";
-        char *token = strtok(line, sep);
-        if(!strcmp(token, "$")) {
-            ls_in_progress = false;
-            token = strtok(NULL, sep);
-            if (!strcmp(token, "cd")) {
-                token = strtok(NULL, sep);
-                current = change_dir(current, token);
-            } else if(!strcmp(token, "ls")) {
-                ls_in_progress = true; 
+        char *first = strtok(line, sep);
+        if(!strcmp(first, "$")) {
+            char *command = strtok(NULL, sep);
+            if (!strcmp(command, "cd")) {
+                char *directory = strtok(NULL, sep);
+                current = change_dir(file_system, current, directory);
             }
-        } else {
-            if(!strcmp(token, "dir")) {
-                token = strtok(NULL, sep);
-                add_node(current, token, -1);
-            }else{
-                char *end;
-                long size = strtol(token, &end, 10);
-                token = strtok(NULL, sep);
-                add_node(current, token, size);
+            else {
+                if(strcmp(command, "ls")) {
+                    fprintf(stderr, "error: unknown command %s\n", command);
+                    return 1;
+                }
+            }
+        }else {
+                if(!strcmp(first, "dir")) {
+                    char *directory = strtok(NULL, sep);
+                    add_node(current, directory, -1);
+                }else{
+                    char *end;
+                    long size = strtol(first, &end, 10);
+                    char *file_name = strtok(NULL, sep);
+                    add_node(current, file_name, size);
+                }
             }
         }
-    }
     fclose(puzzleFile);
     compute_size(file_system->children);
     long sum = sum_dir_at_most(file_system->children, 100000);
@@ -218,5 +181,6 @@ int main(int argc, char *argv[]) {
     long min = LONG_MAX;
     find_smallest_at_least(file_system->children, delete_at_least, &min);
     printf("smallest at least:     %ld\n", min);
+    destroy_node(file_system);
     return 0;
 }
